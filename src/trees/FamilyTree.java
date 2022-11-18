411ba526efb64ea26300ceea4f83387d90517ef1
vienvincent <vincent.vien@sjsu.edu>
2022-11-18 15:07:56 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -31,8 +31,8 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
-            // Add childNode to this node's children list. Also
-            // set childNode's parent to this node.
+            children.add(childNode);
+            childNode.parent = parent;
         }
         
         
@@ -41,31 +41,34 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (parent.getName().equals(targetName))
                 return this;
                     
-            // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+                if (child.getNodeWithName(targetName) != null)
+                	return child;
             }
             
-            // Not found anywhere.
             return null;
         }
         
         
-        // Returns a list of ancestors of this TreeNode, starting with this node’s parent and
+        // Returns a list of ancestors of this TreeNode, starting with this node's parent and
         // ending with the root. Order is from recent to ancient.
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
-            // the nodes of a tree is like traversing a linked list. If that isn’t clear,
+            // the nodes of a tree is like traversing a linked list. If that isn't clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+            TreeNode currNode = parent;
+            while (currNode != null) {
+            	ancestors.add(currNode);
+            	currNode = currNode.parent;
+            }
 
             return ancestors;
         }
