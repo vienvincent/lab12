23a7ecc90bad8c0530aa5a15f02505a67108087f
Kyle Wang <kyle.wang@sjsu.edu>
2022-11-18 16:30:52 -0800

    Updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -32,7 +32,7 @@ public class FamilyTree
         void addChild(TreeNode childNode)
         {
             children.add(childNode);
-            childNode.parent = parent;
+            childNode.parent = this;
         }
         
         
@@ -41,13 +41,13 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (parent.getName().equals(targetName))
+            if (targetName.equals(this.getName()))
                 return this;
                     
             for (TreeNode child: children)
             {
                 if (child.getNodeWithName(targetName) != null)
-                	return child;
+                	return child.getNodeWithName(targetName);
             }
             
             return null;
@@ -127,7 +127,7 @@ public class FamilyTree
 		if (colonIndex < 0)
 			throw new TreeException("Error: Colon not found.");
 		String parent = line.substring(0, colonIndex);
-		String childrenString = line.substring(colonIndex, line.length());
+		String childrenString = line.substring(colonIndex+1);
 		String[] childrenArray = childrenString.split(",");
 		
 		TreeNode parentNode;
@@ -153,16 +153,16 @@ public class FamilyTree
 	TreeNode getMostRecentCommonAncestor(String name1, String name2) throws TreeException
 	{
 		// Get nodes for input names.
-		TreeNode node1 = root.???		// node whose name is name1
+		TreeNode node1 = root.getNodeWithName(name1);		// node whose name is name1
 		if (node1 == null)
-			??? Throw a TreeException with a useful message
-		TreeNode node2 = root.???		// node whose name is name2
+			throw new TreeException("No node with this name");
+		TreeNode node2 = root.getNodeWithName(name2);		// node whose name is name2
 		if (node2 == null)
-			??? Throw TreeException with a useful message
+			throw new TreeException("No node with this name");
 		
 		// Get ancestors of node1 and node2.
-		ArrayList<TreeNode> ancestorsOf1 = ???
-		ArrayList<TreeNode> ancestorsOf2 = ???
+		ArrayList<TreeNode> ancestorsOf1 = node1.collectAncestorsToList();
+		ArrayList<TreeNode> ancestorsOf2 = node2.collectAncestorsToList();
 		
 		// Check members of ancestorsOf1 in order until you find a node that is also
 		// an ancestor of 2. 
